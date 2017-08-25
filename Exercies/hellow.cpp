8b72c3549111b81e0b8c8394cfbe66988ae9abe5
Erik Dubois <raouad@gmail.com>
2017-08-25 11:51:12 +0000

    Some comment on the .cpp file

--- Exercies/hellow.cpp
+++ Exercies/hellow.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
-#include <cmath>
+/*This code is copied from the lectures
+http://compphysics.github.io/ComputationalPhysics/doc/pub/languages/pdf/languages-print.pdf
+and is a simple hello world program that will say Hello
+and calculate sine of a given number*/
+
+#include <iostream>  //input output
+#include <cmath>    //math functions
 //#include <cstdio>
-using namespace std;
+using namespace std;   /*amespaces provide a method for preventing name
+conflicts in large projects. Symbols declared inside a namespace block are placed
+in a named scope that prevents them from being mistaken for identically-named
+symbols in other scopes*/
 int main (int argc, char* argv[])
 {
   double r, s;        // declare variables
